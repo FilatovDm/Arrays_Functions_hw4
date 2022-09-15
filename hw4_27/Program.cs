c434aab840b361db3d6dce9c97d5d4fb58988b36
Dmitriy Filatov <filatov-d@mail.ru>
2022-09-15 21:03:11 +0300

    Task 4_27 completed

---
+++ hw4_27/Program.cs
@@ -0,0 +1,27 @@
+﻿/*
+Задача 27: Напишите программу, которая принимает на вход число и 
+выдаёт сумму цифр в числе.
+452 -> 11
+82 -> 10
+9012 -> 12
+*/
+
+int SumDigits(int number)
+{
+    int sumDigits = 0;
+    int num = Math.Abs(number);
+    
+    while (num != 0)
+    {
+        sumDigits = sumDigits + num % 10;
+        num = num / 10;
+    }
+
+    return sumDigits; 
+}
+
+Console.Write("Введите число: ");
+int imputNumber = int.Parse(Console.ReadLine());
+
+int sum = SumDigits(imputNumber);
+Console.WriteLine($"Сумма всех цифр числа {imputNumber}: {sum}");
